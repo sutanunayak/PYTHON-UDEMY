f8ee128c2aa66abd878c75a04b8758119037a3f8
Sutanu Nayak <sutanunayak11@gmail.com>
2026-03-05 11:12:41 +0530

    Generators in python

---
+++ 08_generators/02_infinite_generators.py
@@ -0,0 +1,15 @@
+def infinite_chai():
+    count = 1
+    while True:
+        yield f"Refil #{count}"
+        count += 1
+
+refill = infinite_chai()
+user2 = infinite_chai()
+
+for _ in range(6):
+    print(next(refill))
+
+
+for _ in range(4):
+    print(next(user2))
---
+++ 08_generators/04_close_generator.py
@@ -0,0 +1,25 @@
+def local_chai():
+    yield "Masala chai"
+    yield "Ginger chai"
+
+def imported_chai():
+    yield "Matcha"
+    yield "Oolong"
+
+def full_menu():
+    yield from local_chai()
+    yield from imported_chai()
+
+for chai in full_menu():
+    print(chai)
+
+def chai_stall():
+    try:
+        while True:
+            order = yield "Waiting for chai order"
+    except:
+        print("Stall closed, No more chai")
+
+stall = chai_stall()
+print(next(stall))
+stall.close() # clean up
