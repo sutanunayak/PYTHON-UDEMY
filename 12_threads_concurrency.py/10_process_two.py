7039fb86e447a99bc77971d72f3fbbe85b0c37ab
Sutanu Nayak <sutanunayak11@gmail.com>
2026-03-10 12:48:29 +0530

    Thread concurrency

---
+++ 12_threads_concurrency.py/10_process_two.py
@@ -0,0 +1,18 @@
+from multiprocessing import Process
+import time
+
+
+def cpu_heavy():
+    print(f"Crunching some number...")
+    total = 0
+    for i in range(10**9):
+        total += i
+    print("Done")
+
+if __name__ == "__main__":
+    start = time.time()
+    processes = [Process(target=cpu_heavy) for _ in range(2)]
+    [t.start() for t in processes]
+    [t.join() for t in processes]
+
+    print((f"Time taken: {time.time() - start:.2f} seconds"))
